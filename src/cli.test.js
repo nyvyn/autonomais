43e1a12577b3414daec13dd37d7241723f3f43ff
Ron Lancaster <2886602+nyvyn@users.noreply.github.com>
2024-04-09 06:53:09 -0500

    Add unit test for CLI script functionality.

---
+++ src/cli.test.js
@@ -0,0 +1,44 @@
+describe("cli", () => {
+    let originalArgv;
+
+    beforeEach(() => {
+        // Remove all cached modules. The cache needs to be cleared before running
+        // each command, otherwise you will see the same results from the command
+        // run in your first test in later tests.
+        jest.resetModules();
+
+        // Each test overwrites process arguments so store the original arguments
+        originalArgv = process.argv;
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+
+        // Set process arguments back to the original value
+        process.argv = originalArgv;
+    });
+
+    it("should run help command", async () => {
+        const consoleSpy = jest.spyOn(console, "log");
+
+        await runCommand("--help");
+
+        expect(consoleSpy).toBeCalledWith("Help");
+    });
+});
+
+/**
+ * Programmatically set arguments and execute the CLI script
+ *
+ * @param {...string} args - positional and option arguments for the command to run
+ */
+async function runCommand(...args) {
+    process.argv = [
+        "node", // Not used, but a value is required at this index in the array
+        "cli.js", // Not used, but a value is required at this index in the array
+        ...args,
+    ];
+
+    // Require the yargs CLI script
+    return require("./cli");
+}
