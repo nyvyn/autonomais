67884dff2b1ba292addb1afc254b5862e0b086cd
Ron Lancaster <2886602+nyvyn@users.noreply.github.com>
2024-04-09 07:42:39 -0500

    aider: Import `src/utils/parseWorkflow.ts` to run it in the default command of cli.js.
    # Aider chat conversation:
    
    USER: import `src/utils/parseWorkflow.ts` to run it in the default command of cli.js
    
    ASSISTANT: ```diff
    --- src/cli.js
    +++ src/cli.js
    @@ ... @@
     const yargs = require("yargs/yargs");
     const {hideBin} = require("yargs/helpers");
    +const { parseWorkflow } = require("./utils/parseWorkflow");
    
     yargs(hideBin(process.argv))
         .command("$0", "The default command", () => {
         }, (argv) => {
    -        parseWorkflow(argv.$0);
    +        parseWorkflow(argv._[0]);
         })
         .option("verbose", {
             alias: "v",
             type: "boolean",
             description: "Run with verbose logging"
         })
         .parse();
    ```

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 const yargs = require("yargs/yargs");
 const {hideBin} = require("yargs/helpers");
+const { parseWorkflow } = require("./utils/parseWorkflow");
 
 yargs(hideBin(process.argv))
     .command("$0", "The default command", () => {
     }, (argv) => {
-        parseWorkflow(argv.$0);
+        parseWorkflow(argv._[0]);
     })
     .option("verbose", {
         alias: "v",
