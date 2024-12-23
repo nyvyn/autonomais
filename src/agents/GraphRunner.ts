420e6e0112f7beff6438fa4418da33e71c95e457
nyvyn <2886602+nyvyn@users.noreply.github.com>
2024-12-22 18:05:29 -0600

    Added initial cli and test, not yet parsing yaml, not yet managing agents.

---
+++ src/agents/GraphRunner.ts
@@ -0,0 +1,55 @@
+import { convertContextToLangChainMessages } from "@/utils/convertContext";
+import { BaseMessage } from "@langchain/core/messages";
+import { Runnable, RunnableConfig } from "@langchain/core/runnables";
+import { Pregel } from "@langchain/langgraph/pregel";
+
+type AgentState = {
+    messages: Array<BaseMessage>;
+    viewstate?: Record<any, any>
+};
+
+export interface GraphRunnerConfig extends RunnableConfig {
+    graph: Pregel;
+}
+
+export class GraphRunner extends Runnable<
+    Readonly<string | [string, string] | [string, string][]>,
+    string
+> {
+    lc_namespace: ["Graph Runner"];
+
+    private readonly graph: Pregel;
+
+    public constructor(config: GraphRunnerConfig) {
+        super(config);
+        this.graph = config.graph;
+    }
+
+    public async invoke(input: Readonly<string | [string, string] | [string, string][]>, options: Partial<RunnableConfig> | undefined): Promise<any> {
+        const messages = convertContextToLangChainMessages(input);
+
+        const output: {
+            messages: BaseMessage[]
+        } = await this.graph.invoke({
+            messages,
+        }, {
+            runName: "Graph Runner - Graph Run",
+            ...options
+        });
+
+        if (!output) return "Invalid response from AI.";
+
+        const lastMessage = output.messages[output.messages.length - 1];
+        let completion: string;
+        if (output.messages.length > 0) {
+            const content = lastMessage.content;
+            if (typeof content === "string") {
+                completion = content;
+            } else {
+                completion = JSON.stringify(content);
+            }
+        }
+
+        return completion || "No response from AI.";
+    }
+}
