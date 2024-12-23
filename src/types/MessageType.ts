2f8cbd767a8ec5141f1840ec4f1c3f70e59b0a34
nyvyn <2886602+nyvyn@users.noreply.github.com>
2024-12-22 18:05:31 -0600

    refactor(removed unused messagetype): removed MessageType

--- src/types/MessageType.ts
+++
@@ -1,6 +0,0 @@
-export const enum MessageType {
-    ApiMessage = "apimessage",
-    UserMessage = "usermessage"
-}
-
-export type MessageContext = Readonly<string | [string, MessageType.ApiMessage | MessageType.UserMessage] | [string, string][]>
