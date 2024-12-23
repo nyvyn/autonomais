2ed3e49fe34951a96dcb4d0b7a004ff1d8ad1c13
nyvyn <2886602+nyvyn@users.noreply.github.com>
2024-12-22 18:05:31 -0600

    refactor: minor package upgrade of langgraph and code convention updates

--- src/types/GraphNode.ts
+++ src/types/GraphNode.ts
@@ -1,9 +1,9 @@
 import { StructuredTool } from "@langchain/core/tools";
 
 export type GraphNode = {
-    name: string;
-    instructions: string;
-    isConditional: boolean;
-    isExit: boolean;
-    tools?: StructuredTool[];
-}
+  name: string;
+  instructions: string;
+  isConditional: boolean;
+  isExit: boolean;
+  tools?: StructuredTool[];
+};
